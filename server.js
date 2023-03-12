b0546b2b202a3a221069116ee2b81c196ac10fdd
tihunn <74502721+tihunn@users.noreply.github.com>
2023-03-12 18:45:36 +0300

    add server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const path = require("path")
+
+
+const PORT = process.env.REACT_APP_PORT || 80
+
+const app = express()
+
+app.use(express.static(__dirname))
+app.use(express.static(path.resolve(__dirname, "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
+app.listen( PORT, () => {
+    console.log(`Server listens PORT: ${PORT}`)
+    console.log(`REACT_APP_API_URL: ${process.env.REACT_APP_API_URL}`)
+})
